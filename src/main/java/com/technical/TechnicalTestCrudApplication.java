017521b1fc1586f6b40e6f426a1f10d73a86554f
Deybi Montenegro <deibymontenegrob@gmail.com>
2022-09-26 23:25:48 -0500

    initial commit

---
+++ src/main/java/com/technical/TechnicalTestCrudApplication.java
@@ -0,0 +1,13 @@
+package com.technical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTestCrudApplication.class, args);
+	}
+
+}
